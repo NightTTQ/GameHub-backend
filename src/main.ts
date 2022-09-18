5f582047db6f791ed34c5dc5bbd5474285bb666b
TTQ <1932170807@qq.com>
2022-09-18 10:07:30 +0800

    加入swaggerAPI文档

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle("GameHub API Document")
+    .setDescription("GameHub API document")
+    .setVersion("1.0")
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerOptions);
+  SwaggerModule.setup("doc", app, document);
   await app.listen(7002);
 }
 bootstrap();

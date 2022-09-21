e0387e82ee807a12e3f15d116f9a766ff541a3f4
TTQ <1932170807@qq.com>
2022-09-21 22:10:16 +0800

    加入全局异常捕获与返回拦截器

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { HttpExceptionFilter } from "./exception.filter";
+import { TransformInterceptor } from "./interceptor/transform.interceptor";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new TransformInterceptor());
   const swaggerOptions = new DocumentBuilder()
     .setTitle("GameHub API Document")
     .setDescription("GameHub API document")

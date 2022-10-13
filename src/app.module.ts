e51a8693c276d8fcb8e9f6b9c3d4c853d66a0ce8
TTQ <1932170807@qq.com>
2022-10-13 17:46:05 +0800

    加入请求logger中间件

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from "@nestjs/common";
+import { Module, MiddlewareConsumer, RequestMethod } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { UserModule } from "./user/user.module";
 
 import { MongooseModule } from "@nestjs/mongoose";
-import { AuthModule } from './auth/auth.module';
+import { LoggerMiddleware } from "./common/middleware/logger.middleware";
+import { AuthModule } from "./auth/auth.module";
 
 @Module({
   imports: [
@@ -17,4 +18,11 @@ import { AuthModule } from './auth/auth.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .exclude({ path: "test", method: RequestMethod.POST })
+      .forRoutes("");
+  }
+}

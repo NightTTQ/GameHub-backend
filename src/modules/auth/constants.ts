89cfe130ee5ad498beea91fa5328bab0f0e1c26c
TTQ <1932170807@qq.com>
2022-11-21 19:39:22 +0800

    修改编译设置将secrets文件夹进行打包

--- src/modules/auth/constants.ts
+++ src/modules/auth/constants.ts
@@ -1,10 +1,10 @@
 import { readFileSync } from "fs";
 import { join } from "path";
 
-const key = readFileSync(join(__dirname, "../../../src/secrets/private.pem"), {
+const key = readFileSync(join(__dirname, "../../secrets/private.pem"), {
   encoding: "utf-8",
 });
-const cert = readFileSync(join(__dirname, "../../../src/secrets/public.pem"), {
+const cert = readFileSync(join(__dirname, "../../secrets/public.pem"), {
   encoding: "utf-8",
 });
 
